a7f062722cb44021def9734faee9f637b1b2e503
Cenk Gokturk <cenkgokturk06@gmail.com>
2021-02-04 14:19:05 +0300

    User authentication has been added

---
+++ Controllers/UserLoginController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data;
+using Microsoft.Data.SqlClient;
+using ASPNETAOP.Models;
+
+namespace ASPNETAOP.Controllers
+{
+    public class UserLoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //[isAuthenticated]
+        [HttpPost]
+        public IActionResult Login(UserLogin ur)
+        {
+            String connection = "Data Source=DESKTOP-II1M7LK;Initial Catalog=AccountDb;Integrated Security=True";
+            using (SqlConnection sqlconn = new SqlConnection(connection))
+            {
+                string sqlquery = "select '" + ur.Userpassword + "' from AccountInfo where Usermail = '" + ur.Usermail + "' ";
+                using (SqlCommand sqlcomm = new SqlCommand(sqlquery, sqlconn))
+                {
+                    sqlconn.Open();
+                    SqlDataReader reader = sqlcomm.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine("{0}", reader.GetString(0));
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No rows found.");
+                    }
+                    reader.Close();
+                }
+            }
+            return View(ur);
+        }
+    }
+}
---
+++ Controllers/UserRegistrationController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data;
+using Microsoft.Data.SqlClient;
+using ASPNETAOP.Models;
+
+namespace ASPNETAOP.Controllers
+{
+    public class UserRegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(UserRegister ur)
+        {
+            String connection = "Data Source=DESKTOP-II1M7LK;Initial Catalog=AccountDb;Integrated Security=True";
+            using(SqlConnection sqlconn = new SqlConnection(connection))
+            {
+                string sqlquery = "insert into AccountInfo(Username, Usermail, Userpassword) values ('" + ur.Username + "', '" + ur.Usermail + "', '" + ur.Userpassword + "' )";
+                using (SqlCommand sqlcomm = new SqlCommand(sqlquery, sqlconn))
+                {
+                    sqlconn.Open();
+                    sqlcomm.ExecuteNonQuery();
+                    ViewData["Message"] = "New User "+ur.Username+ " is saved successfully";
+                }
+            }
+            return View(ur);
+        }
+    }
+}
