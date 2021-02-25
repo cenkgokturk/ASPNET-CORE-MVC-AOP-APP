d71c8a2a2082c902413b332a81d7241f4e942a6f
Cenk Gokturk <cenkgokturk06@gmail.com>
2021-02-25 23:30:22 +0300

    Predefined user roles for newly registered users

--- Controllers/UserRegistrationController.cs
+++ Controllers/UserRegistrationController.cs
@@ -26,13 +26,60 @@ namespace ASPNETAOP.Controllers
             return View();
         }
 
+        private int GetUsedID(String Usermail)
+        {
+            Console.WriteLine("Get UserID Entry");
+            int UserID = -1;
+
+            String connection = _configuration.GetConnectionString("localDatabase");
+            using (SqlConnection sqlconn = new SqlConnection(connection))
+            {
+                string sqlquery = "select UserID  from AccountInfo where Usermail = '" + Usermail + "' ";
+                using (SqlCommand sqlcomm = new SqlCommand(sqlquery, sqlconn))
+                {
+                    sqlconn.Open();
+                    SqlDataReader reader = sqlcomm.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            UserID = reader.GetInt32(0);
+                        }
+                    }
+
+                    sqlconn.Close();
+                }
+            }
+
+            Console.WriteLine("Get UserID Close");
+
+            return UserID;
+        }
+
+        private void AddUserRole(int UserID)
+        {
+            String connection = _configuration.GetConnectionString("localDatabase");
+            using (SqlConnection sqlconn = new SqlConnection(connection))
+            {
+                string sqlquery = "insert into UserRoles(UserID, Roleid) values ('" + UserID + "', 2)";
+                using (SqlCommand sqlcomm = new SqlCommand(sqlquery, sqlconn))
+                {
+                    sqlconn.Open();
+                    sqlcomm.ExecuteNonQuery();
+                }
+            }
+
+            Console.WriteLine("UserROle added");
+        }
+
         [HttpPost]
         public IActionResult Create(UserRegister ur)
         {
             //Necessary to prevent sessionID from changing with every request
             HttpContext.Session.Set("What", new byte[] { 1, 2, 3, 4, 5 });
 
-            //Add new user to the database
+            //Add a new user to the database
             String connection = _configuration.GetConnectionString("localDatabase");
             using (SqlConnection sqlconn = new SqlConnection(connection))
             {
@@ -41,11 +88,22 @@ namespace ASPNETAOP.Controllers
                 {
                     sqlconn.Open();
                     sqlcomm.ExecuteNonQuery();
+
                     ViewData["Message"] = "New User "+ur.Username+ " is saved successfully";
+
+                    sqlconn.Close();
                 }
             }
 
+            //retrieve the UserID of the newly created user
+            int UserID = GetUsedID(ur.Usermail);
+
+            Console.WriteLine("Add UserROles");
+            //define a standart permission
+            AddUserRole(UserID);
+
             return View(ur);
         }
+
     }
 }
